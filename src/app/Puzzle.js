6f200c424a2646db747bb775dc27e87043482467
wynter22 <dev.u0lee@gmail.com>
2022-03-30 00:27:05 +0900

    puzzle 구현

--- src/app/Puzzle.js
+++ src/app/Puzzle.js
@@ -1,54 +1,66 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../asset/Puzzle.css';
 
 export default function Puzzle() {
   const originPuzzle = [1, 2, 0, 4, 5, 6, 3, 7, 9, 10, 11, 8, 13, 14, 15, 12];
   const [puzzle, setPuzzle] = useState([]);
+  const [sortedPuzzle, setSortedPuzzle] = useState([]);
 
-  let sortedPuzzle = [];
-  let zeroItem = -1;
+  const [zeroItem, setZeroItem] = useState(-1);
 
   useEffect(() => {
     init();
   }, []);
 
-  function init() {
-    sortedPuzzle = [];
-    setPuzzle([...originPuzzle]);
-    const temp = [...originPuzzle].sort((a, b) => a - b);
-    temp.push(temp.shift());
-    sortedPuzzle = temp;
-  }
-
   useEffect(() => {
-    zeroItem = puzzle.indexOf(0);
+    setZeroItem(puzzle.indexOf(0));
+    finishPuzzle();
   }, [puzzle]);
+
+  function init() {
+    setPuzzle([...originPuzzle]);
+    setSortedPuzzle(() => {
+      const temp = [...originPuzzle];
+      temp.sort((a, b) => a - b);
+      temp.push(temp.shift());
+      return temp;
+    });
+  }
 
   function movePuzzle(item, targetIdx) {
     const diff = Math.abs(targetIdx - zeroItem);
     if (diff === 4 || diff === 1) {
-      const temp = [...puzzle];
-      temp.splice(zeroItem, 1, item);
-      temp.splice(targetIdx, 1, 0);
+      setPuzzle(puzzle => {
+        const temp = [...puzzle];
+        temp.splice(zeroItem, 1, item);
+        temp.splice(targetIdx, 1, 0);
+        return temp;
+      });
     }
-
-    finishPuzzle();
   }
 
-  const finishPuzzle = useCallback(() => {
-    if (puzzle.length && sortedPuzzle.length && puzzle.equals(sortedPuzzle)) {
-      alert('The End!!');
+  function finishPuzzle() {
+    const isSortedPuzzle = puzzle.every(
+      (puzzleItem, idx) => puzzleItem === sortedPuzzle[idx]
+    );
+
+    if (isSortedPuzzle && puzzle.length) {
+      alert('퍼즐을 완료했습니다!!');
       init();
     }
-  }, [puzzle, sortedPuzzle]);
+  }
 
-  return <div className="puzzle">
-    {
-      puzzle.map((item, idx) => {
-        return item > 0
-          ? <div key={idx} className="item" onClick={e => movePuzzle(item, idx)}>{item}</div>
-          : <div key={idx} className="empty"></div>;
-      })
-    }
-  </div>;
+  return (
+    <div className="puzzle">
+      {puzzle.map((item, idx) => {
+        return item > 0 ? (
+          <div key={idx} className="item" onClick={e => movePuzzle(item, idx)}>
+            {item}
+          </div>
+        ) : (
+          <div key={idx} className="empty"></div>
+        );
+      })}
+    </div>
+  );
 }
